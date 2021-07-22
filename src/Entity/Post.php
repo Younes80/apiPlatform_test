fa004c852e3b2b3fd988a66553e7d79a1104a06c
Younes80 <57212570+Younes80@users.noreply.github.com>
2021-07-22 09:02:34 +0200

    first commit

---
+++ src/Entity/Post.php
@@ -0,0 +1,234 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\PostRepository;
+use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
+use App\Controller\PostCountController;
+use App\Controller\PostPublishController;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints\Length;
+
+/**
+ * @ORM\Entity(repositoryClass=PostRepository::class)
+ */
+#[ApiResource(
+    normalizationContext: ['groups' => ['read:collection']],
+    denormalizationContext: ['groups' => 'write:Post'],
+    paginationItemsPerPage: 2,
+    paginationMaximumItemsPerPage: 2,
+    paginationClientItemsPerPage: true,
+    collectionOperations: [
+        'get',
+        'post' => [
+            'validation_groups' => [Post::class, 'validationGroups']
+        ],
+        'count' => [
+            'method' => 'GET',
+            'path' => '/posts/count',
+            'controller' => PostCountController::class,
+            'read' => false,
+            'pagination_enabled' => false,
+            'filters' => [],
+            'openapi_context' => [
+                'summary' => 'Récupère le nombre total d\'article',
+                'parameters' => [
+                    [
+                        'in' => 'query',
+                        'name' => 'online',
+                        'shema' => [
+                            'type' => 'integer',
+                            'maximum' => 1,
+                            'minimum' => 0
+                        ],
+                        'description' => 'Filtre les articles en ligne'
+                    ]
+                ],
+                'responses' => [
+                    '200' => [
+                        'description' => 'OK',
+                        'content' => [
+                            'application/json' => [
+                                'shema' => [
+                                    'type' => 'integer',
+                                    'example' => 3
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ]
+    ],
+    itemOperations: [
+        'put',
+        'delete',
+        'get' => [
+            'normalization_context' => [
+                'groups' => ['read:collection', 'read:item', 'read:Post'],
+                'openapi_definition_name' => 'Détail'
+            ]
+        ],
+        'publish' => [
+            'method' => 'POST',
+            'path' => '/posts/{id}/publish',
+            'controller' => PostPublishController::class
+
+        ]
+    ]
+), ApiFilter(SearchFilter::class, properties: ['id' => 'exact', 'title' => 'partial'])]
+class Post
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    #[Groups(['read:collection'])]
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    #[Groups(['read:collection', 'write:Post']), Length(min: 5, groups: ['create:Post'])]
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    #[Groups(['read:collection', 'write:Post'])]
+    private $slug;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    #[Groups(['read:item', 'write:Post'])]
+    private $content;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    #[Groups(['read:item'])]
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $updatedAt;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Category::class, inversedBy="posts")
+     */
+    #[Groups(['read:item', 'write:Post'])]
+    private $category;
+
+    /**
+     * @ORM\Column(type="boolean", options={"defautlt"="0"})
+     */
+    #[Groups(['read:collection'])]
+    private $online = false;
+
+    public static function validationGroups(self $post)
+    {
+        return ['create:Post'];
+    }
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTime();
+        $this->updatedAt = new \DateTime();
+    }
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    public function getOnline(): ?bool
+    {
+        return $this->online;
+    }
+
+    public function setOnline(bool $online): self
+    {
+        $this->online = $online;
+
+        return $this;
+    }
+}
